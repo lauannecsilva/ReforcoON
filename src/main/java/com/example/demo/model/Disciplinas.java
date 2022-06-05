c5d63a4483ed0f9bc56e6b555173b6395be9b1c0
lauannecsilva <lauannefarias@gmail.com>
2022-06-05 19:17:47 -0300

    Nova alteração

--- src/main/java/com/example/demo/model/Disciplinas.java
+++ src/main/java/com/example/demo/model/Disciplinas.java
@@ -6,4 +6,18 @@ public class Disciplinas {
 
     public String nome;
     public int numero;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    
 }
